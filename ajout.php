014e908f1ef8c6b6759182f4e257ed69d968dd61
Abdessalam-Elboukri <abdessalamelboukri00@gmail.com>
2022-01-31 23:25:29 +0100

    ajoute la fonctionaliter ajoute un itulisateur

---
+++ ajout.php
@@ -0,0 +1,81 @@
+<?php
+	// session_start();
+	if(isset($_POST['submit'])){
+		$data = file_get_contents('users.json');
+		$data_array = json_decode($data);
+		//data in our POST
+		$input = array(
+			'Nom'    =>     $_POST['Nom'],  
+            'Email'  =>     $_POST['Email'],  
+            'Phone'  =>     $_POST['Phone'] ,
+            'Enroll' =>     $_POST['Enroll'] ,
+            'Date_A' =>     $_POST['Date_A'],
+		);
+		//append the POST data
+		$data_array[] = $input;
+		//return to json and put contents to our file
+		$data_array = json_encode($data_array, JSON_PRETTY_PRINT);
+		file_put_contents('users.json', $data_array);
+		$_SESSION['message'] = 'Data successfully appended';
+	}
+	else{
+		$_SESSION['message'] = 'Fill up add form first';
+	}
+	// header('location:index.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajoute | e-class</title>
+    <link rel="stylesheet" href="bootstrap5/css/bootstrap.css">
+    <script src="bootstrap5/js/bootstrap.min.js"></script>
+    <script src="bootstrap5/js/bootstrap.bundle.js"></script>
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main class="vh-100 d-flex justify-content-center align-items-center back" >
+        <div class="col-lg-6 col-sm-8 col-md-7 ">
+            <div class=" shadow bg-light p-3 " style="border-radius:16px">
+                <div class="d-flex flex-column ">
+                    <div class="text-center">
+                        <h1 >Ajoute un utilisateur</h1>
+                        <a href="students.php" class="position-absolute" style="left: 10px;">X</a>
+                    </div>
+                    <form method="POST" action="ajout.php"  class="gap-5">
+                        <div class="d-flex flex-column">
+                            <label for="" class="fs-5">Name</label>
+                            <input type="text" name="Nom" id="nom" >
+                        </div>
+                        <div class="d-flex flex-column">
+                            <label for="" class="fs-5">E-mail</label>
+                            <input type="email" name="Email" id="email">
+                        </div>
+                        <div class="d-flex flex-column">
+                            <label for="" class="fs-5">Phone Number</label>
+                            <input type="number" name="Phone" id="phone">
+                        </div>
+                        <div class="d-flex flex-column">
+                            <label for="" class="fs-5">Enroll Number</label>
+                            <input type="number" name="Enroll" id="enroll">
+                        </div>
+                        <div class="d-flex flex-column">
+                            <label for="" class="fs-5">Date of admission</label>
+                            <input type="date" name="Date_A" id="Date_A">
+                        </div>
+                        <div class="mt-2"> 
+                            <input id="submit" type="submit" name="submit" value="Ajoute" class="px-4 pt-2 pb-2 bg-primary border-0 text-white rounded-2"   > 
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
+
--- students.php
+++ students.php
@@ -31,7 +31,7 @@
                         <h2 class=" style = font-size: 1.5rem">Students List</h2>
                         <div style="font-size: 1rem;">
                             <i class="fa fa-sort text-primary" aria-hidden="true"></i>
-                            <a href="#" class="btn btn-primary text-uppercase ds-6">add new student</a>
+                            <a href="ajout.php" class="btn btn-primary text-uppercase ds-6">add new student</a>
                         </div>
                     </div>
                     <hr class="m-auto" style="width: 100%;">
@@ -55,18 +55,18 @@
                             
                             $stud_info = 'users.json';
                             $data = file_get_contents($stud_info);
-                            $info = json_decode($data, true);
+                            $info = json_decode($data);
                             
                             foreach ( $info as $val ) {
                                 echo "<tr class=\"mt-3 \" >
                                         <td scope=\"row\">
                                             <img src=\"./images/students.jpg\"  width=\"50\" height=\"50\">
                                         </td>
-                                        <td class=\"align-middle \">{$val['Nom']}</td>
-                                        <td class=\"align-middle\">{$val['Email']}</td>
-                                        <td class=\"align-middle\">{$val['Phone']}</td>
-                                        <td class=\"align-middle\">{$val['Enroll']}</td>
-                                        <td class=\"align-middle\">{$val['Date_A']}</td>
+                                        <td class=\"align-middle \"> $val->Nom</td>
+                                        <td class=\"align-middle\">$val->Email</td>
+                                        <td class=\"align-middle\">$val->Phone</td>
+                                        <td class=\"align-middle\">$val->Enroll</td>
+                                        <td class=\"align-middle\">$val->Date_A</td>
                                         <td class=\"text-primary align-middle \">
                                         <div class=\"d-flex flex-nowrap gap-3\">
                                             <i class=\"fa fa-pencil \" aria-hidden=\"true\"></i>
